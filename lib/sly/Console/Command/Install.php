07fb2c108728d87d30f4d82e29b39a3825940df1
Christoph <github@xrstf.de>
2013-02-16 04:21:07 +0100

    some first clumsy attempts to make the install command do something

--- lib/sly/Console/Command/Install.php
+++ lib/sly/Console/Command/Install.php
@@ -39,6 +39,9 @@ class sly_Console_Command_Install extends Base {
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$container = $this->getContainer();
 
+		// load our language file
+		$container->getI18N()->appendFile(SLY_SALLYFOLDER.'/setup/lang');
+
 		// check system config and stop with an error page if any serious problems arise
 		$params = array('errors' => false);
 		$params = sly_Util_Setup::checkSystem($params);
@@ -46,9 +49,100 @@ class sly_Console_Command_Install extends Base {
 		$params = sly_Util_Setup::checkDirectories($params);
 		$params = sly_Util_Setup::checkHttpAccess($params);
 
+		$output->writeln('System Check');
+		$output->writeln('------------');
+		$output->writeln('');
+
+		//////////////////////////////////////////////////////////////////////////
+		// explain PHP config
+
+		$results = $params['results'];
+		$exts    = $params['exts'];
+
+		sort($exts);
+		$extensions = array();
+
+		foreach ($exts as $ext) {
+			$extensions[] = $this->renderResult($results['ext_'.$ext], $ext);
+		}
+
+		$output->writeln('  PHP');
+		$output->writeln('    version         : '.$this->renderResult($results['version']));
+		$output->writeln('    memory limit    : '.$this->renderResult($results['mem_limit']));
+		$output->writeln('    register_globals: '.$this->renderResult($results['register_globals']));
+		$output->writeln('    magic_quotes    : '.$this->renderResult($results['magic_quotes']));
+		$output->writeln('    safe_mode       : '.$this->renderResult($results['safe_mode']));
+		$output->writeln('    open_basedir    : '.$this->renderResult($results['open_basedir']));
+		$output->writeln('    extensions      : '.implode(', ', $extensions));
+
+		//////////////////////////////////////////////////////////////////////////
+		// show PDO driver status
+
+		if (!empty($params['pdoDrivers'])) {
+			$output->writeln('');
+			$output->writeln('    <error>No PDO drivers found!</error>');
+			$output->writeln('    '.strip_tags(t('pdo_driver_help')));
+		}
+		else {
+			$output->writeln('    PDO drivers     : <info>'.implode('</info>, <info>', $params['availPdoDrivers']).'</info>');
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		// show directory status
+
+		$output->writeln('');
+		$output->writeln('  Directories');
+
+		if (!empty($params['directories'])) {
+			$output->writeln('');
+			$output->writeln('    <error>Not all required directories could be created!</error>');
+
+			foreach ($params['directories'] as $dir) {
+				$output->writeln('     - '.$dir);
+			}
+		}
+		else {
+			$output->writeln('    <info>all directories are okay</info>');
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		// show HTTP access protection status
+
+		$output->writeln('');
+		$output->writeln('  HTTP access protection');
+
+		if (!empty($params['httpAccess'])) {
+			$output->writeln('');
+			$output->writeln('    <error>Not all required directories could be protected against HTTP access!</error>');
+
+			foreach ($params['httpAccess'] as $dir) {
+				$output->writeln('     - '.$dir);
+			}
+
+			$output->writeln('    Make sure to put an .htaccess file in the directories named above,');
+			$output->writeln('    otherwise private data is accessible for anyone on the web.');
+		}
+		else {
+			$output->writeln('    <info>all directories are protected</info>');
+		}
+
+		$output->writeln('');
+
 		if ($params['errors']) {
-			$this->syscheckView($params);
 			return 1;
 		}
+	}
+
+	protected function renderResult(array $result, $showRange = true) {
+		return str_replace(
+			array('<warning>', '</warning>', '<success>', '</success>'),
+			array('<comment>', '</comment>', '<info>', '</info>'),
+			sly_Util_Setup::getWidget(
+				$result, null, $showRange,
+				'<{tclass}>{text}</{tclass}>',
+				'<{tclass}>{text}</{tclass}> ({tooltip})',
+				false
+			)
+		);
 	}
 }
