558febad07a37614f15f8f7f8d38f0d453ff2676
Christoph <github@xrstf.de>
2013-07-17 04:14:43 +0200

    updated for the new dispatcher model

--- lib/sly/Dispatcher/Setup.php
+++ lib/sly/Dispatcher/Setup.php
@@ -10,13 +10,48 @@
 
 class sly_Dispatcher_Setup extends sly_Dispatcher {
 	/**
-	 * Constructor
+	 * return the DI container identifier for a controller
 	 *
-	 * @param sly_Container $container
-	 * @param string        $containerClassPrefix
+	 * This identifier controls both how controllers are found and how their
+	 * created instaces are stored. The implementation should not check for any
+	 * existing service, but rather only check if $name is syntactically valid
+	 * and then return the identifier.
+	 *
+	 * @param  string $name  controller name, e.g. 'login'
+	 * @return string        identifier, e.g. 'sly-controller-backend-login'
 	 */
-	public function __construct(sly_Container $container, $containerClassPrefix) {
-		parent::__construct($container, $containerClassPrefix, true);
+	public function getControllerIdentifier($name) {
+		return 'sly-setup-controller-'.strtolower($name);
+	}
+
+	/**
+	 * create a controller instance
+	 *
+	 * This method is called if no container has been defined in the service. It
+	 * should contruct the class name and then instantiate the controller.
+	 *
+	 * @param  string $name              controller name, e.g. 'login'
+	 * @return sly_Controller_Interface  controller instance
+	 */
+	protected function buildController($name) {
+		// controller names make start with a number because we have our prefix,
+		// so sly_Controller_23 is perfectly valid (but still stupid). They are
+		// not allowed to start or end with an underscore, however.
+		if (mb_strlen($name) === 0 || !preg_match('#^[0-9a-z][0-9a-z_]*$#is', $name) || mb_substr($name, -1) === '_') {
+			throw new sly_Controller_Exception(t('unknown_controller', $name), 404);
+		}
+
+		$className = 'sly_Controller_Setup';
+		$parts     = explode('_', strtolower($name));
+
+		foreach ($parts as $part) {
+			$className .= '_'.ucfirst($part);
+		}
+
+		// class exists and is not abstract?
+		$this->checkControllerClass($className);
+
+		return new $className();
 	}
 
 	/**
